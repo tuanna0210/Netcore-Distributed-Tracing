ea5e08b0d6fdd6c63b3230ee11f8e7ade5e93c38
Tuan Nguyen <anhtuann0210@gmail.com>
2024-09-12 14:38:40 +0700

    Initialize projects

---
+++ src/Services/ServiceA/Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WelcomeController(IHttpClientFactory clientFactory) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var client = clientFactory.CreateClient("ServiceB");
+            var serviceBResponse = await client.GetStringAsync("api/welcome");
+            return "Welcome from Service A!" + "\n" + serviceBResponse;
+        }
+    }
+}
---
+++ src/Services/ServiceB/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceB.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Welcome from Service B!";
+        }
+    }
+}
